8fcc44fdead0d456739540f1ab571af03fda7e88
Vighnesh Sanjay Dalvi <98831399+VighneshSanjayDalvi@users.noreply.github.com>
2023-03-20 23:36:48 +0530

    Hover to see pattern
    PATTERN:
    ****
    ***
    **
    *

---
+++ Starpatternthree.java
@@ -0,0 +1,27 @@
+public class Starpatternthree {
+
+    public static void main(String args[]){
+        
+
+        // ****
+        // ***
+        // **
+        // *
+        
+        System.out.println("****\n***\n**\n*\n\n\n");
+
+        for(int i=1; i<=4;i++){
+            for(int j=1;j<=4;j++){
+                if(j<=5-i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
